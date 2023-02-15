da6af475a5a8a472559c9eef5a987539d2472e3c
victor.guidi <vsantosg4072@gmail.com>
2023-02-15 18:07:51 -0300

    Added debug for typescript and javascript too

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -70,5 +70,35 @@ return {
     }
     dap.configurations.c = dap.configurations.cpp
     dap.configurations.rust = dap.configurations.cpp
+
+    dap.adapters.node2 = {
+      type = "executable",
+      command = "node",
+      args = { os.getenv("HOME") .. "/.dev/msdebuggers/vscode-node-debug2/out/src/nodeDebug.js" },
+    }
+
+    for _, language in ipairs({ "javascript", "typescript" }) do
+      dap.configurations[language] = {
+        {
+          name = "Launch",
+          type = "node2",
+          request = "launch",
+          program = "${file}",
+          cwd = vim.fn.getcwd(),
+          sourceMaps = true,
+          protocol = "inspector",
+          console = "integratedTerminal",
+          outFiles = { "${workspaceFolder}/**/dist/**/*.js" },
+          envFile = "${workspaceFolder}/backend/.env",
+        },
+        {
+          -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+          name = "Attach to process",
+          type = "node2",
+          request = "attach",
+          processId = require("dap.utils").pick_process,
+        },
+      }
+    end
   end,
 }
